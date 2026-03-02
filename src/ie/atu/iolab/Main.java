cc47fc2c45c07232ca7326d7b915b240aec4c9c8
Min Kyaw <mintheinkyawprd@gmail.com>
2026-03-02 14:26:30 +0000

    Remove old Main class and add new Main class with file existence check for input.txt

---
+++ src/ie/atu/iolab/Main.java
@@ -0,0 +1,18 @@
+package ie.atu.iolab;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+public class Main {
+    public static void main(String[] args) {
+        Path projectRoot = Paths.get(System.getProperty("user.dir"));
+        Path inputFilePath = projectRoot.resolve("resources").resolve("input.txt");
+
+        if (Files.exists(inputFilePath)) {
+            System.out.println("input.txt found at: " + inputFilePath.toAbsolutePath());
+        } else {
+            System.out.println("input.txt not found at: " + inputFilePath.toAbsolutePath());
+        }
+    }
+}
